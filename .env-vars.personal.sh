da616d7e70c1ec7c1bc72260a08b9c06f49e763a
Anthony Sceresini <asceresini@gmail.com>
2015-12-01 11:36:43 +1100

    default docker machine update

--- .env-vars.personal.sh
+++ .env-vars.personal.sh
@@ -1,6 +1,6 @@
 ANDROID_HOME=/usr/local/opt/android-sdk
 EDITOR="vi"
-DOCKER_MACHINE_NAME=dev1
+DOCKER_MACHINE_NAME=dev
 DOCKER_IP=$(dm url $DOCKER_MACHINE_NAME | egrep -o "(\d+\.)+\d+")
 
 #docker machine session setup
