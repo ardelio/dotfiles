ca46350b042172ddf11910827cfe601c0f1d463b
Anthony Sceresini <asceresini@gmail.com>
2017-03-06 10:29:30 +1100

    update devtools version

--- .aliases.myob.sh
+++ .aliases.myob.sh
@@ -1,5 +1,5 @@
 alias fly_tax='fly -t taxonline'
 function fly_tax_prune_workers() { fly_tax workers | grep stalled | awk '{print $1;}' | xargs -n 1 fly -t taxonline prune-worker -w; };
 
-export DEVTOOLS_VERSION=0.12.0
+export DEVTOOLS_VERSION=0.13.0
 . ~/code/taxonline-devops/helpers/aliases.sh
