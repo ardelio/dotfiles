0d4e1d075d50efa334acc0a6d5c8fb8c3e9649a7
Anthony Sceresini <asceresini@gmail.com>
2017-03-24 14:13:42 +1100

    change repo name in helper

--- .aliases.myob.sh
+++ .aliases.myob.sh
@@ -2,4 +2,4 @@ alias fly_tax='fly -t taxonline'
 function fly_tax_prune_workers() { fly_tax workers | grep stalled | awk '{print $1;}' | xargs -n 1 fly -t taxonline prune-worker -w; };
 
 export DEVTOOLS_VERSION=latest
-. ~/code/taxonline-devops/helpers/aliases.sh
+. ~/code/taxonline-devtools/helpers/aliases.sh
