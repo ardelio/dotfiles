729ac5a73863d03b5b654ab674296ec60a40db54
Anthony Sceresini <asceresini@gmail.com>
2017-05-20 16:21:01 +1000

    fix up structure. strip out mac and xps

---
+++ zsh/mac_settings.zsh
@@ -0,0 +1,11 @@
+export PATH="/usr/local/bin:$PATH"
+
+
+eval "$(rbenv init -)"
+. /usr/local/etc/profile.d/z.sh
+
+# added by travis gem
+[ -f /Users/anthony.sceresini/.travis/travis.sh ] && source /Users/anthony.sceresini/.travis/travis.sh
+
+alias title='export DISABLE_AUTO_TITLE=true && title'
+alias gpp='git status | fpp'
