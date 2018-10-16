23b8a0fd81fc1f48ff294e9c387a70b487e7979b
Anthony Sceresini <asceresini@gmail.com>
2018-10-17 09:14:57 +1100

    More changes

--- .aliases.myob.sh
+++ .aliases.myob.sh
@@ -6,9 +6,9 @@ alias taxprofile_dump="dynamodbexportcsv --awsregion 'ap-southeast-2' --table 'T
 export DEVTOOLS_VERSION=latest
 . ~/code/taxonline-devtools/helpers/aliases.sh
 
-alias stack_aim='osascript /Users/anthony.sceresini/code/AD-OnlineTax-Common/stack-starter/stackStarter.applescript "NZ Aim" false true'
-alias stack_egst='osascript /Users/anthony.sceresini/code/AD-OnlineTax-Common/stack-starter/stackStarter.applescript "NZ Egst" false true'
-alias stack_bas_arl='osascript /Users/anthony.sceresini/code/AD-OnlineTax-Common/stack-starter/stackStarter.applescript "AU Bas" false true'
-alias stack_bas_po='osascript /Users/anthony.sceresini/code/AD-OnlineTax-Common/stack-starter/stackStarter.applescript "AU PO" false true'
+alias tax_db_conx_prod='AWS_PROFILE=qprod aws s3 cp s3://myob-ex-taxonline-prod-assets/terraform/prod/taxonline-taxapi-db/output.json - | jq -r "\"psql -h \" + .taxonline_tax_api_db_host + \" -d \" + .taxonline_tax_api_db_name + \" -U \" + .taxonline_tax_api_db_user + \" # \" + .taxonline_tax_api_db_password" | pbcopy'
 
-alias tax_db_conx_prod='AWS_PROFILE=prod aws s3 cp s3://myob-ex-taxonline-prod-assets/terraform/prod/taxonline-taxapi-db/output.json - | jq -r "\"psql -h \" + .taxonline_tax_api_db_host + \" -d \" + .taxonline_tax_api_db_name + \" -U \" + .taxonline_tax_api_db_user + \" # \" + .taxonline_tax_api_db_password" | pbcopy'
+alias awslogin_quasar_dev='myob-auth login --username `whoami` --role adfs-ex-taxonline-development-admin'
+alias awslogin_quasar_prod='myob-auth login --username `whoami` --role adfs-ex-taxonline-prod-admin --profile qprod'
+alias awslogin_fe_dev='myob-auth login --username `whoami` --role adfs-acc-workpapers-and-compliance-development-admin'
+alias awslogin_fe_prod='myob-auth login --username `whoami` --role adfs-acc-workpapers-and-compliance-production-admin --profile feprod'
