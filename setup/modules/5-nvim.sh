de7e4ccee3d209e82ee3510b893efef11d33d043
Anthony Sceresini <anthony.sceresini@myob.com>
2020-02-10 15:52:30 +1100

    Fix NVIM

--- setup/modules/5-nvim.sh
+++ setup/modules/5-nvim.sh
@@ -5,7 +5,8 @@ if hash nvim 2>/dev/null; then
 else
     echo NVIM not found...installing
     brew install neovim
+    python2 -m pip install neovim
     python3 -m pip install neovim
-    ls -d ~/.config/nvim || ln -s ~/.dotfiles/nvim ~/.config/.
+    ls -d ~/.config/nvim || ( mkdir -p $HOME/.config && ln -s ~/.dotfiles/nvim ~/.config/. )
     nvim +PlugInstall +qa
 fi
